fe40de42e32a92c82655af59f453be022d211bb2
Nandeesh <nandeeshv0001@gmail.com>
2024-01-04 16:01:15 +0530

    added dummy code.

--- sourcecode/demosonar.py
+++ sourcecode/demosonar.py
@@ -1,31 +1,81 @@
-import os
-import requests
-import sys
+import pdfkit
+import cairosvg
+from html2image import Html2Image
+hti = Html2Image()
 
-TOKEN= str(sys.argv[1])
-OWNER= str(sys.argv[2])
-REPO= str(sys.argv[3])
-workflow_name= str(sys.argv[4])
-parameter1= str(sys.argv[5])
-parameter2 = str(sys.argv[6])
+def convert_html_file_to_pdf(html_file_path, output_file_path, type, output_pdf_file_path):
+    """
+    Convert HTML file to PDF.
 
-print( "the toke value is")
-def trigger_workflow(workflow_name,parameter1,parameter2):
+    Args:
+        html_file_path (str): Path to the HTML file.
+        output_file_path (str): Path to save the output PDF file.
+        
+    """
 
-      headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": f"token {TOKEN}",
-      }
+    if(type == "pdf"):
+        try:
+            options = {
+                'page-size': 'A4',
+                'margin-top': '0mm',
+                'margin-right': '0mm',
+                'margin-bottom': '0mm',
+                'margin-left': '0mm',
+            }
+            print("html_file_path>>>>>>>>>>", html_file_path)
+            pdfkit.from_file(html_file_path, output_file_path, options=options)
+            print(f'PDF successfully created at: {output_file_path}')
+        except FileNotFoundError as e:
+            print(f'HTML file not found: {html_file_path}')
+        except Exception as e:
+            print(f'Error converting HTML to PDF: {e}')
 
-      data = {
-        "event_type": workflow_name,
-        "client_payload": {
-          'parameter1': parameter1,
-          'parameter2': parameter2
-        }
-      }
+    elif(type == "svg"):
+        #write a code to convert html to svg
+        # try:
+        #     with open(html_file_path, 'rb') as html_file:
+        #         svg_data = cairosvg.svg2svg(file_obj=html_file)
+        
+        #     with open(output_file_path, 'wb') as svg_file:
+        #         svg_file.write(svg_data)
 
-      responsevalue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
-      print(responsevalue.content)
+        #     print(f'SVG successfully created at: {output_file_path}')
+        # except FileNotFoundError as e:
+        #     print(f'HTML file not found: {html_file_path}')
+        # except Exception as e:
+        #     print(f'Error converting HTML to SVG: {e}')
+         #Assuming hti.screenshot captures HTML content and saves it to an image
+        #hti.screenshot(html_file=html_file_path, save_as='temp_img.svg')
+    
+        # Convert the temporary image to SVG using cairosvg
+        # cairosvg.svg2svg(url='temp_img.png', write_to=output_file_path)
+        # try:
+        #     cairosvg.svg2svg(url=html_file_path, write_to=output_file_path)
+        # except Exception as e:
+        #  print(f"Error converting image to SVG: {e}")
+        from weasyprint import HTML
+        HTML(filename=html_file_path).write_pdf(output_pdf_file_path)
+        from cairosvg import svg2svg
 
-trigger_workflow(workflow_name,parameter1,parameter2)
+        # Convert PDF to SVG using cairosvg
+        svg2svg(url=output_pdf_file_path, write_to=output_file_path)
+
+
+
+        
+
+    elif(type == "img"):
+        #write a code to convert html to img
+        hti.screenshot(html_file=html_file_path,save_as='blue_page.png')
+        
+
+if __name__ == "__main__":
+    type = "svg"
+    # Input HTML file path
+    html_file_path = "/home/nandeesh/Desktop/demo_django/mypro/myapp/input.html"
+    output_pdf_file_path = "/home/nandeesh/Desktop/demo_django/mypro/myapp/output.pdf"
+
+    # Output PDF file path
+    output_file_path = "/home/nandeesh/Desktop/demo_django/mypro/myapp/output.svg"
+
+    convert_html_file_to_pdf(html_file_path, output_file_path, type, output_pdf_file_path)
